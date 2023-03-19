4150277a870e6b9114748d2fdeae4ca99d351061
Guillermo Riaño <guilledark17@gmail.com>
2023-03-19 11:39:39 -0600

    ✅ Adding: Services - Contracts

---
+++ BackEndAPI/BackEndAPI/Services/Contract/IDepartmentService.cs
@@ -0,0 +1,10 @@
+﻿using BackEndAPI.Models;
+
+namespace BackEndAPI.Services.Contract
+{
+    public interface IDepartmentService
+    {
+        // Method to Get Department List
+        Task<List<Department>> GetDepartmentList();
+    }
+}
---
+++ BackEndAPI/BackEndAPI/Services/Contract/IEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using BackEndAPI.Models;
+
+namespace BackEndAPI.Services.Contract
+{
+    public interface IEmployeeService
+    {
+        // Method to Get Employee List
+        Task<List<Employee>> GetEmployeeList();
+
+        // Method to Get Employee by id
+        Task<Employee> GetEmployeeById(int employeeId);
+
+        // Method to Add Employee
+        Task<Employee> AddEmployee(Employee model);
+
+        // Method to Update Employee
+        Task<bool> UpdateEmployee(Employee model);
+
+        // Method to Delete Employee
+        Task<bool> DeleteEmployee(Employee model);
+    }
+}
