1147f2201ce78160cc13c34caa354bb099a1395d
Guillermo Riaño <guilledark17@gmail.com>
2023-03-19 11:40:01 -0600

    ✅ Adding: Services - Implementation

---
+++ BackEndAPI/BackEndAPI/Services/Implementation/DepartmentService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackEndAPI.Models;
+using BackEndAPI.Services.Contract;
+
+namespace BackEndAPI.Services.Implementation
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private DbAngularapiCrudContext _dbContext;
+
+        public DepartmentService(DbAngularapiCrudContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        // Method to Get Department List
+        public async Task<List<Department>> GetDepartmentList()
+        {
+            try { 
+                List<Department> list = new List<Department>();
+
+                list = await _dbContext.Departments.ToListAsync();
+
+                return list;
+            }catch (Exception ex) {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ BackEndAPI/BackEndAPI/Services/Implementation/EmployeeService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackEndAPI.Models;
+using BackEndAPI.Services.Contract;
+
+namespace BackEndAPI.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private DbAngularapiCrudContext _dbContext;
+
+        public EmployeeService(DbAngularapiCrudContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        // Method to Get Employee List
+        public async Task<List<Employee>> GetEmployeeList()
+        {
+            try
+            {
+                List<Employee> list = new List<Employee>();
+
+                list = await _dbContext.Employees.Include(dpt => dpt.DepartmentId).ToListAsync();
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Method to Get Employee by id
+        public async Task<Employee> GetEmployeeById(int employeeId)
+        {
+            try
+            {
+                Employee? employee = new Employee();    // Can to return null with "?"
+
+                employee = await _dbContext.Employees.Include(dpt => dpt.DepartmentId)
+                    .Where(e => e.EmployeeId == employeeId).FirstOrDefaultAsync();
+
+                return employee;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Method to Add Employee
+        public async Task<Employee> AddEmployee(Employee model)
+        {
+            try
+            {
+                _dbContext.Employees.Add(model);
+                await _dbContext.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Method to Update Employee
+        public async Task<bool> UpdateEmployee(Employee model)
+        {
+            try
+            {
+                _dbContext.Employees.Update(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Method to Delete Employee
+        public async Task<bool> DeleteEmployee(Employee model)
+        {
+            try
+            {
+                _dbContext.Employees.Remove(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
